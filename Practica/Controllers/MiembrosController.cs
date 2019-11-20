4e6dfdccc0dc99fcd486f618f761cd66f4754c67
pamelafv26x <57980218+pamelafv26x@users.noreply.github.com>
2019-11-20 18:32:23 -0400

    Miembro Controller Added

---
+++ Practica/Controllers/MiembrosController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica.Models;
+namespace Practica.Controllers
+{
+    public class MiembrosController : Controller
+    {
+        Practica5Entities db = new Practica5Entities();
+
+        // GET: Miembros
+        [HttpGet]
+        public ActionResult GetMembers()
+        {
+
+            var members = db.Miembros.Where(m => m.estado_asig == 1).ToList();
+            ViewBag.Members = db.Miembros.Select(x => new SelectListItem { Text = x.nombre_miembro, Value = x.ID.ToString()});
+
+            return View(members);
+        }
+
+        [HttpPost]
+        public ActionResult AddMembers(Miembro model)
+        {
+            var members = new Miembro();
+
+            if (ModelState.IsValid)
+            {
+                 members = new Miembro() {
+                     nombre_miembro = model.nombre_miembro,
+                     id_asig = model.id_asig,
+                     estado_asig = model.estado_asig
+                };
+                db.Miembros.Add(members);
+
+                return RedirectToAction("GetMembers");
+            }
+           
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult AddMembers()
+        {
+
+            ViewBag.Asignaturas = db.Asignaturas.Select(a => new SelectListItem()
+            {
+                Text = a.nombre_asig,
+                Value = a.ID.ToString()
+            });
+
+            ViewBag.Estado = db.Estados.Select(e => new SelectListItem() {
+
+                Text = e.nombre_estado,
+                Value = e.ID.ToString()
+            });
+
+            return View();
+        }
+    }
+}
---
+++ Practica/ViewModels/MiembroViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica.ViewModels
+{
+    public class MiembroViewModel
+    {
+    }
+}
