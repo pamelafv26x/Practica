5f11d04a8092b06db3438c5b4b1f34fa644c6622
Rosangel <miranda1401@hotmail.com>
2019-11-20 20:10:36 -0400

    Las partes que realice fueron los dos modulos de la agenda y el lector de archivos.

---
+++ Practica/Controllers/ContactosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica.Models;
+
+namespace Practica.Controllers
+{
+    public class ContactosController : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Contactos
+        public ActionResult Index(string busqueda)
+        {
+            var lista = from x in db.Contactos
+                        select x;
+
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                return View(db.Contactos.ToList());
+
+            }
+            else
+            {
+                lista = lista.Where(a => (a.Nombre.Contains(busqueda))
+                || (a.Celular.Contains(busqueda)));
+                return View(lista);
+            }
+        }
+
+        // GET: Contactos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // GET: Contactos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contactos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Celular,Email,Direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contactos.Add(contactos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactos);
+        }
+
+        // GET: Contactos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contactos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Celular,Email,Direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactos);
+        }
+
+        // GET: Contactos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contactos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contactos contactos = db.Contactos.Find(id);
+            db.Contactos.Remove(contactos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
